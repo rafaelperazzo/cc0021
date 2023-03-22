4f5a046c7497a048ff95a4cc290f525615e05be3
Rafael Perazzo <rafaelperazzo@gmail.com>
2023-03-22 14:39:41 -0300

    template omp

---
+++ codigo/template_openmp.c
@@ -0,0 +1,46 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+
+float *gerar_vetor(int n);
+
+void mostrar_vetor(float *v,int tamanho);
+
+int main() {
+    /*
+    INICIO
+    */
+    time_t t;
+    srand(time(NULL));
+    float *vetor = NULL;
+    vetor = gerar_vetor(MAX);
+
+    
+
+    /*
+    FIM
+    */
+    return 0;
+}
+
+float *gerar_vetor(int n) {
+    float *vetor;
+    int i;
+    vetor = (float *)malloc(sizeof(float) * n);
+    for (i=0;i<n;i++) {
+        float num = (rand() / (float)RAND_MAX);
+        vetor[i] = num;
+    }
+    return vetor;
+}
+
+void mostrar_vetor(float *v,int tamanho) {
+    int i;
+    for (i=0;i<tamanho;i++) {
+        printf("[%.5f]",v[i]);
+    }
+    printf("\n");
+}
