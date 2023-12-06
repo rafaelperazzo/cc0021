3094dc3765a04e89b8ec2c88306452930e5a1616
Rafael Mota <rafael.mota@ufca.edu.br>
2023-12-06 11:08:51 -0300

    argumento inteiro

--- codigo/pThreads/thread2_argumentos.c
+++ codigo/pThreads/thread2_argumentos.c
@@ -10,11 +10,15 @@ struct argumentos
 };
 
 void *tarefa();
+void *outra_tarefa(void *parametro);
 
 int main()
 {
-    pthread_t thread1, thread2;
-    int  iret1, iret2;
+    pthread_t thread1, thread2,thread3;
+    int  iret1, iret2,iret3;
+    int *valor = malloc(sizeof(*valor));
+    *valor = 10;
+
     struct argumentos args1,args2;
 
     args1.valor1 = 10;
@@ -25,10 +29,11 @@ int main()
 
     iret1 = pthread_create( &thread1, NULL, tarefa, (void*) &args1);
     iret2 = pthread_create( &thread2, NULL, tarefa, (void*) &args2);
+    iret3 = pthread_create( &thread3, NULL, outra_tarefa, valor);
 
     pthread_join( thread1, NULL);
     pthread_join( thread2, NULL); 
-
+    free(valor);
     return (0);
 }
 
@@ -40,4 +45,13 @@ void *tarefa(void *args)
      }
      printf("%d\n", ((struct argumentos *)args)->valor1);
      printf("%f\n", ((struct argumentos *)args)->valor2);
+}
+
+void *outra_tarefa(void *parametro)
+{
+     int i;
+     for (i=1;i<=1000000000;i++) {
+
+     }
+     printf("Tarefa3: %d\n", *((int*) parametro));
 }
