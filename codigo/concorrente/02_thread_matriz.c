c515969bcc4b249cbfbd580fea021d35bff48ace
Rafael Perazzo <rafael.mota@ufca.edu.br>
2024-04-11 13:59:13 -0300

    corrigido codigo base para soma de linhas em c

--- codigo/concorrente/02_thread_matriz.c
+++ codigo/concorrente/02_thread_matriz.c
@@ -6,29 +6,11 @@
 #define LINHAS 3
 #define COLUNAS 3
 
-int **gerar_matriz(int linhas, int colunas) {
-    int **matriz = (int **) malloc(linhas * sizeof(int *));
-    for (int i = 0; i < linhas; i++) {
-        matriz[i] = (int *) malloc(colunas * sizeof(int));
-        for (int j = 0; j < colunas; j++) {
-            matriz[i][j] = rand() % 100;
-        }
-    }
-    return matriz;
-}
+int **gerar_matriz(int linhas, int colunas);
 
-void mostrar_matriz(int **matriz, int linhas, int colunas) {
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-}
+void mostrar_matriz(int **matriz, int linhas, int colunas);
 
-void *tarefa(void *linha) {
-    
-}
+void *tarefa(void *linha);
 
 int **matriz;
 
@@ -51,4 +33,28 @@ int main() {
     }
     
     return 0;
+}
+
+int **gerar_matriz(int linhas, int colunas) {
+    int **matriz = (int **) malloc(linhas * sizeof(int *));
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int *) malloc(colunas * sizeof(int));
+        for (int j = 0; j < colunas; j++) {
+            matriz[i][j] = rand() % 100;
+        }
+    }
+    return matriz;
+}
+
+void mostrar_matriz(int **matriz, int linhas, int colunas) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void *tarefa(void *linha) {
+    
 }
