51fa5632ab9c71add75a80103c3f361ce90e04fa
Rafael Perazzo <rafael.mota@ufca.edu.br>
2024-04-16 10:05:56 -0300

    resolucao aula 05 - soma total da matriz

---
+++ codigo/concorrente/exercicio_aula_06.c
@@ -0,0 +1,73 @@
+/*
+Escreva um programa que realize o cálculo das somas dos valores das linhas de uma matriz qualquer de números inteiros e 
+imprima os resultados na tela. 
+Faça com que o cálculo do somatório de cada linha seja realizado concorrentemente por uma thread. 
+A soma de cada linha deve ser mostrada por cada thread. 
+
+*/
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LINHAS 3
+#define COLUNAS 3
+
+int **gerar_matriz(int linhas, int colunas);
+void mostrar_matriz(int **matriz, int linhas, int colunas);
+void *tarefa(void *linha);
+
+int **matriz;
+
+int main() {
+    
+    srand(time(NULL));
+    matriz = gerar_matriz(LINHAS, COLUNAS);
+    int rc;
+    mostrar_matriz(matriz, LINHAS, COLUNAS);
+
+    pthread_t threads[LINHAS];
+
+    int i;
+
+    for (i=0; i<LINHAS; i++) {
+        rc = pthread_create(&threads[i], NULL, tarefa, &i);
+    }
+
+    for (int i=0; i<LINHAS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    
+    free(matriz);
+    return 0;
+}
+
+int **gerar_matriz(int linhas, int colunas) {
+    int **matriz = (int **) malloc(linhas * sizeof(int *));
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int *) malloc(colunas * sizeof(int));
+        for (int j = 0; j < colunas; j++) {
+            matriz[i][j] = rand() % 100;
+        }
+    }
+    return matriz;
+}
+
+void mostrar_matriz(int **matriz, int linhas, int colunas) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void *tarefa(void *linha) {
+    int soma_linha = 0;
+    int LINHA = *(int*)linha;
+    printf("Linha --->: %d\n", LINHA);
+    for (int j = 0; j < COLUNAS; j++) {
+        soma_linha += matriz[LINHA][j];
+    }
+    printf("Soma da linha %d: %d\n", LINHA, soma_linha);
+}
